97a0eba760a587fb60dc3cca06e58e300ee8f5f6
Anton Krat <threesundaysonewerk@gmail.com>
2023-05-17 09:29:39 +0800

    add project in github

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '../index.css';
+import JS_IMAGE from '../assets/JS.jpg';
+
+const jsImageHTML = document.createElement('img');
+jsImageHTML.src = JS_IMAGE;
+
+document.body.append(jsImageHTML);
